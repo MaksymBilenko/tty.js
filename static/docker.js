8fd81ff1cbdb742574604c84502aa2b5963fbc1c
Maksym Bilenko <Maksym_Bilenko@epam.com>
2016-05-13 18:54:13 +0300

    Add console logging

--- static/docker.js
+++ static/docker.js
@@ -6,7 +6,8 @@ function createButtons() {
       success: function (containers) {
         $.each(containers, function () {
           if (this != '') {
-            var button = '<button class="container" onclick="new tty.Window(false, {title:\''+this+'\', protocol:\'docker\', container:\''+this+'\'})"></button>' +
+            var button =
+                '<button class="container" onclick="new tty.Window(false, {title:\''+this+'\', protocol:\'docker\', container:\''+this+'\'})"></button>' +
                 '<button class="logs" onclick="new tty.Window(false, {title:\''+this+' logs\', protocol:\'dockerLogs\', container:\''+this+'\'})"></button>'+this+'<br/>';
             console.log(button);
             $(".dockerContainers").append(button);
