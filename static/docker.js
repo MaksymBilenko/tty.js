6b23eb9ea19a6fc8be11018b6452482ac4818616
Maksym Bilenko <Maksym_Bilenko@epam.com>
2016-05-13 15:05:45 +0300

    Add comment for logs window

--- static/docker.js
+++ static/docker.js
@@ -7,7 +7,7 @@ function createButtons() {
         $.each(containers, function () {
           if (this != '') {
             var button = '<button class="container" onclick="new tty.Window(false, {title:\''+this+'\', protocol:\'docker\', container:\''+this+'\'})"></button>' +
-                '<button class="logs" onclick="new tty.Window(false, {title:\''+this+'\', protocol:\'dockerLogs\', container:\''+this+'\'})"></button>'+this+'<br/>';
+                '<button class="logs" onclick="new tty.Window(false, {title:\''+this+' logs\', protocol:\'dockerLogs\', container:\''+this+'\'})"></button>'+this+'<br/>';
             console.log(button);
             $(".dockerContainers").append(button);
           }
